aaafc70e2c5c8cb08198811ed28fb96d84be95c4
Joremy <Joremy@DESKTOP-23AQAC6>
2017-09-17 16:16:35 +0800

    Add Stub method in case of reference file does not exist

--- SearchStringInFilesTests/CtrlTests.cs
+++ SearchStringInFilesTests/CtrlTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using SearchStringInFiles.Controller;
 using SearchStringInFiles.Enum;
+using SearchStringInFilesTests.Stub;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,25 @@ namespace SearchStringInFiles.Controller.Tests
     {
         Ctrl ctrl = new Ctrl();
         [Test()]
-        public void InTimeRangeTest()
+        public void InTimeRangeTest_TimeRangeAll()
         {
-            string filePath = @"";
+            string filePath = @"\\vmware-host\Shared Folders\文件\JMS-Feedback\feedback_24600.xml";
             TimeRange tr = TimeRange.All;
             var expected = true;
 
+            var actual = ctrl.InTimeRange(filePath, tr);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test()]
+        public void InTimeRangeTest_TimeRangeOneMonth()
+        {
+            string filePath = @"";
+            TimeRange tr = TimeRange.OneMonth;
+            var expected = true;
+
+            ctrl = new CtrlStub();
             var actual = ctrl.InTimeRange(filePath, tr);
 
             Assert.AreEqual(expected, actual);
---
+++ SearchStringInFilesTests/Stub/CtrlStub.cs
@@ -0,0 +1,16 @@
+﻿using SearchStringInFiles.Controller;
+using System;
+using System.IO;
+
+namespace SearchStringInFilesTests.Stub
+{
+    class CtrlStub : Ctrl
+    {
+        public override DateTime GetFileTime(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return DateTime.Now;
+            return base.GetFileTime(filePath);
+        }
+    }
+}
